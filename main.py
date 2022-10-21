e75ad549e209e619c46a167323f373810d5051b2
Bassam AL SAEDY <basaidi@gmail.com>
2022-10-21 14:35:26 +0200

    save new img

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ img = cv2.imread('assets/troopy.jpg', 1)
 img = cv2.resize(img, (0, 0), fx=0.5, fy=0.5)
 img = cv2.rotate(img, cv2.ROTATE_90_COUNTERCLOCKWISE)
 
+cv2.imwrite('new_img.jpg', img)
 
 cv2.imshow('Image', img)
 cv2.waitKey(0)
